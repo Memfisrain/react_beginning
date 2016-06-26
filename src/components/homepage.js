f1f8a9e01fc2b819ec5f5a3180f97ae53704240e
Memfis <nikitamemfis@gmail.com>
2016-06-27 00:22:37 +0400

    initial commit

---
+++ src/components/homepage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require("react");
+
+var Home = React.createClass({
+	render: function() {
+		return (
+				<div className="jumbotron">
+					<h1>Pluralsight administration</h1>
+					<p>React, react Router, and Flux for ultra responsive web apps</p>
+				</div>
+			);
+	}
+});
+
+module.exports = Home;
