55a136b5e9ea03318e06848df830c4751d292f6f
Memfis <nikitamemfis@gmail.com>
2016-06-29 23:45:09 +0400

    added authorForm and manageAuthorPage

---
+++ src/components/authors/authorForm.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var React = require("react");
+var Input = require("../common/textInput");
+
+var AuthorForm = React.createClass({
+	render: function() {
+		return (
+			<form>
+				<h1>Manage Author</h1>
+		
+				<Input name="firstName"
+					label="First name"
+					value={this.props.author.firstName}
+					onChange={this.props.onChange} />
+				
+				<Input name="lastName"
+					label="Last name"
+					value={this.props.author.lastName}
+					onChange={this.props.onChange} />
+
+				<input type="submit" className="btn btn-primary" value="create" />
+			</form>
+		)
+	}
+});
+
+module.exports = AuthorForm;
